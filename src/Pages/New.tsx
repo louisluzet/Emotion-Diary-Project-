5daa728da1c9da42b77c9887001d67334f8ff5ae
louisluzet <louisluzet@naver.com>
2022-07-14 23:35:47 +0900

    new component proceeding

--- src/Pages/New.tsx
+++ src/Pages/New.tsx
@@ -1,9 +1,42 @@
+import MyButton from "../components/MyButton";
+import MyHeader from "../components/MyHeader";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const getStringDate = (date: any) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
-    return (
-        <div>
-            <h1>New</h1>
-            <p>일기 작성 페이지</p>
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  const [date, setDate] = useState(getStringDate(new Date()));
+  return (
+    <div>
+      <MyHeader
+        headText={"새로운 일기쓰기"}
+        leftChild={
+          <MyButton
+            type={"default"}
+            text={"뒤로가기"}
+            onClick={() => navigate(-1)}
+          />
+        }
+        rightChild={() => {}}
+      />
+      <div>
+        <section>
+          <h4>Today is?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 export default New;
